76b25cc68f0d04b196a55eeb82291dc597dc5e59
ilia blinderman <iblinderman@iblinderman-macbookpro2.roam.corp.google.com>
2019-04-27 03:11:50 -0400

    misc

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+let thesisData;
 let data;
 let $svg;
 let $chart;
@@ -25,20 +26,28 @@ function handleStep(){
     const stepNum = +d3.select(this).at('id').split('-')[1]
     
     if(stepNum===1){
-        $sentences
-            .transition()
-            .st('font-size',10)
-        
-        $chartPara
-            .st('line-height','0.7')
+       
     }
     else if (stepNum===2){
+
+
+        $sentences
+        .st('background-color',d=>{
+          //   console.log(+d.yr);
+            console.log(likelihoodScale(d.yr));
+            console.log(colScale(likelihoodScale(d.yr)))
+  
+            return (colScale(likelihoodScale(d.yr)))
+        });
+
+
         $sentences
         .transition()
-        .duration(1000)
-        .st('font-size',d=>{
-                return d.yr>20? '0': '10'
-            })
+        .st('font-size',10)
+    
+    $chartPara
+        .st('line-height','0.7')
+   
         // .st('display',d=>{ 
         //     return d.yr>20? 'none': 'visible'
         // })
@@ -46,18 +55,25 @@ function handleStep(){
     else if (stepNum===3){
         $sentences
         .transition()
+        .duration(1000)
+        .st('font-size',d=>{
+                return d.yr>20? '0': '10'
+            })
+    }
+    else if (stepNum===4){
+        $sentences
+        .transition()
         .st('display',d=>{
             return d.yr>15? 'none': 'visible'
         })
     }
-    else if (stepNum===4){
+    else if (stepNum===5){
         $sentences
         .transition()
         .st('display',d=>{
             return d.yr>10? 'none': 'visible'
         })
     }
-    else if (stepNum===5){}
     else if (stepNum===6){}
 }
 
@@ -100,29 +116,26 @@ function setupDOM() {
     $sentences = sentencesJoin.append('span.sentence');
   
     $sentences.text(d => d.sentence)
-      .st('background-color',d=>{
-        //   console.log(+d.yr);
-          console.log(likelihoodScale(d.yr));
-          console.log(colScale(likelihoodScale(d.yr)))
-
-          return (colScale(likelihoodScale(d.yr)))
-      });
 
     $steps.on('click',handleStep)
   }
   
   function init() {
     return new Promise((resolve, reject) => {
-      const bumpDataFile = 'article_data.csv';
-      const bumpDataPath = `assets/data/${bumpDataFile}`;
+        
+      const thesisFile= 'assets/data/thesis_chunked.csv'
+      const articleFile = 'assets/data/article_data.csv';
+     
+      const dataFiles = [articleFile, thesisFile] 
   
-      d3.loadData(bumpDataPath, (error, response) => {
+      d3.loadData(...dataFiles, (error, response) => {
         if (error) {
           reject(error);
         } else {
           data = cleanData(response[0]);
+        //   thesisData = response[0]
   
-          console.log(data)
+        //   console.log(data)
 
           resize();
           setupDOM();
