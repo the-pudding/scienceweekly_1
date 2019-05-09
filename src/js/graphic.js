0a38564542be3307d3778f77c06617479def5421
Jan Diehm <jadiehm@gmail.com>
2019-05-09 10:37:13 -0500

    design sprint

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -10,6 +10,8 @@ let $chartStatic;
 let $chartStaticImg;
 let $chartStaticTitle;
 let $chartDynamic;
+let $chartDynamicLegend;
+let $chartDynamicContainer;
 let $chartPara;
 let $story;
 
@@ -24,6 +26,8 @@ let $legendItemsText;
 let currentStep = 1
 let $backButton;
 let $fwdButton;
+let $backOverlay;
+let $fwdOverlay;
 let $slideIndicator;
 
 let sentencesJoin;
@@ -39,13 +43,16 @@ const $arrow = d3.select('.arrow')
 const $coverRight = d3.select('.cover-right')
 const $coverLeft = d3.select('.cover-left')
 const $btnfwd = d3.select('.btn-fwd')
+const $slideContents = d3.select('.chart-static')
+const $storyContents = d3.select('.story')
+const $navBar = d3.select('.nav-bar')
 
 /* global d3 */
 
 function handleCoverClick(){
     //d3.select('body').style('overflow', 'visible')
-
     d3.select('section.story').classed('hidden', false)
+    $navBar.classed('hidden', false)
 
     $coverRight.classed('slide', true)
     $coverLeft.classed('slide', true)
@@ -64,14 +71,16 @@ function updateCopy(){
 
 function updateStepIndicator(){
 
-  console.log(currentStep)
     $slideIndicator
         .text(`${currentStep}/10`)
 
     if(currentStep===1){
         $chartStatic.classed('hidden', true)
-        $chartDynamic.classed('hidden', true)
+        $chartDynamicContainer.classed('hidden', true)
         $btnfwd.classed('is-disabled', false)
+        $slideContents.classed('is-chart', false)
+        $storyContents.classed('is-chart', false)
+        $navBar.classed('is-chart', false)
 
         $chartPara
         .st('line-height','1.65')
@@ -81,8 +90,11 @@ function updateStepIndicator(){
 
     if(currentStep===2){
         $chartStatic.classed('hidden', false)
-        $chartDynamic.classed('hidden', true)
+        $chartDynamicContainer.classed('hidden', true)
         $btnfwd.classed('is-disabled', false)
+        $slideContents.classed('is-chart', false)
+        $storyContents.classed('is-chart', false)
+        $navBar.classed('is-chart', false)
 
         $chartPara
         .st('line-height','1.65')
@@ -91,8 +103,11 @@ function updateStepIndicator(){
     }
     if(currentStep===3){
         $chartStatic.classed('hidden', true)
-        $chartDynamic.classed('hidden', true)
+        $chartDynamicContainer.classed('hidden', true)
         $btnfwd.classed('is-disabled', false)
+        $slideContents.classed('is-chart', false)
+        $storyContents.classed('is-chart', false)
+        $navBar.classed('is-chart', false)
 
         $chartPara
         .st('line-height','1.65')
@@ -101,8 +116,11 @@ function updateStepIndicator(){
     }
     if(currentStep===4){
         $chartStatic.classed('hidden', true)
-        $chartDynamic.classed('hidden', true)
+        $chartDynamicContainer.classed('hidden', true)
         $btnfwd.classed('is-disabled', false)
+        $slideContents.classed('is-chart', true)
+        $storyContents.classed('is-chart', true)
+        $navBar.classed('is-chart', true)
 
         $chartPara
         .st('line-height','1.65')
@@ -111,8 +129,11 @@ function updateStepIndicator(){
     }
     if(currentStep===5){
         $chartStatic.classed('hidden', true)
-        $chartDynamic.classed('hidden', true)
+        $chartDynamicContainer.classed('hidden', true)
         $btnfwd.classed('is-disabled', false)
+        $slideContents.classed('is-chart', false)
+        $storyContents.classed('is-chart', false)
+        $navBar.classed('is-chart', false)
 
         $chartPara
         .st('line-height','1.65')
@@ -121,42 +142,22 @@ function updateStepIndicator(){
     }
     if(currentStep===6){
         $chartStatic.classed('hidden', true)
-        $chartDynamic.classed('hidden', false)
+        $chartDynamicContainer.classed('hidden', false)
         $btnfwd.classed('is-disabled', false)
+        $slideContents.classed('is-chart', true)
+        $storyContents.classed('is-chart', true)
+        $navBar.classed('is-chart', true)
+        $chartDynamicLegend.classed('is-visible', false)
 
         $chartPara
-        .st('line-height','0.6')
+        .st('line-height','0.8')
 
         $sentences
-        .st('font-size',6)
+        .st('font-size',8)
 
         $sentences
         .st('background-color','#FFFFFF')
         .st('color','#282828')
-
-        updateCopy()
-    }
-
-    if(currentStep===7){
-        $chartStatic.classed('hidden', true)
-        $chartDynamic.classed('hidden', false)
-        $btnfwd.classed('is-disabled', false)
-
-        $chartPara
-        .st('line-height','0.6')
-
-        $sentences
-        .st('font-size',6)
-
-        $sentences
-        .transition(2000)
-        .delay((d, i) => i * 5)
-        .st('background-color',d=>colScale(likelihoodScale(d.yr)))
-        .st('color',d=>colScale(likelihoodScale(d.yr)))
-        .st('display','visible')
-
-        $sentences
-        .classed('hidden',false)
 
         $legendTitle
             .classed('hidden',false)
@@ -169,14 +170,46 @@ function updateStepIndicator(){
         $legendItemsText
             .classed('hidden',false)
 
+        updateCopy()
+    }
+
+    if(currentStep===7){
+        $chartStatic.classed('hidden', true)
+        $chartDynamicContainer.classed('hidden', false)
+        $btnfwd.classed('is-disabled', false)
+        $slideContents.classed('is-chart', true)
+        $storyContents.classed('is-chart', true)
+        $navBar.classed('is-chart', true)
+        $chartDynamicLegend.classed('is-visible', true)
+
+        $chartPara
+        .st('line-height','0.8')
+
+        $sentences
+        .st('font-size',8)
+
+        $sentences
+        .transition(2000)
+        .delay((d, i) => i * 5)
+        .st('background-color',d=>colScale(likelihoodScale(d.yr)))
+        .st('color',d=>colScale(likelihoodScale(d.yr)))
+        .st('display','visible')
+
+        $sentences
+        .classed('hidden',false)
+
         paraHeight=d3.select('.sentence-box').st('height')
 
         updateCopy()
     }
     if(currentStep===8){
         $chartStatic.classed('hidden', true)
-        $chartDynamic.classed('hidden', false)
+        $chartDynamicContainer.classed('hidden', false)
         $btnfwd.classed('is-disabled', false)
+        $slideContents.classed('is-chart', true)
+        $storyContents.classed('is-chart', true)
+        $navBar.classed('is-chart', true)
+        $chartDynamicLegend.classed('is-visible', true)
 
         $sentences
         .classed('hidden',d=>{
@@ -192,8 +225,13 @@ function updateStepIndicator(){
     }
     if(currentStep===9){
         $chartStatic.classed('hidden', true)
-        $chartDynamic.classed('hidden', true)
+        $chartDynamicContainer.classed('hidden', true)
         $btnfwd.classed('is-disabled', false)
+        $slideContents.classed('is-chart', false)
+        $storyContents.classed('is-chart', false)
+        $storyContents.classed('is-methods', false)
+        $navBar.classed('is-chart', false)
+        $chartDynamicLegend.classed('is-visible', false)
 
         updateCopy()
 
@@ -205,8 +243,13 @@ function updateStepIndicator(){
     }
     if(currentStep===10){
         $chartStatic.classed('hidden', true)
-        $chartDynamic.classed('hidden', true)
+        $chartDynamicContainer.classed('hidden', true)
         $btnfwd.classed('is-disabled', true)
+        $slideContents.classed('is-chart', false)
+        $storyContents.classed('is-chart', false)
+        $storyContents.classed('is-methods', true)
+        $navBar.classed('is-chart', false)
+
         updateCopy()
 
     }
@@ -217,6 +260,7 @@ function handleClickBack(){
     if (currentStep===1){
       $coverRight.classed('slide', false)
       $coverLeft.classed('slide', false)
+      $navBar.classed('hidden', true)
       d3.select('.intro').transition().style('display', 'block')
       //d3.select('body').style('overflow', 'hidden')
     }
@@ -256,13 +300,17 @@ function resize() {
 function setupDOM() {
 
     $chartStatic = d3.select('div.chart-static-container');
-    $chartDynamic = d3.select('div.chart-dynamic');
+    $chartDynamicContainer = d3.select('div.chart-dynamic')
+    $chartDynamic = d3.select('div.chart-dynamic-chart');
+    $chartDynamicLegend = d3.select('div.chart-dynamic-legend');
 
     $chartStaticTitle = d3.select('p.chart-static-title')
     $chartStaticImg = $chartStatic.select('img')
 
     $prose = d3.select('div.prose')
 
+    $backOverlay = d3.select('.overlay__left')
+    $fwdOverlay = d3.select('.overlay__right')
     $backButton = d3.select('.btn-back')
     $fwdButton = d3.select('.btn-fwd')
     $slideIndicator = d3.select('.current-slide-num')
@@ -273,7 +321,6 @@ function setupDOM() {
   function render() {
 
 
-    $arrow.on('click', handleCoverClick)
 
     $chartPara = $chartDynamic
         .append('p.sentence-box')
@@ -286,11 +333,14 @@ function setupDOM() {
     $sentences = sentencesJoin.append('span.sentence');
     $sentences.text(d => d.sentence)
 
+    $arrow.on('click', handleCoverClick)
     $backButton.on('click',handleClickBack)
     $fwdButton.on('click',handleClickForward)
+    $backOverlay.on('click',handleClickBack)
+    $fwdOverlay.on('click',handleClickForward)
 
-    $legendTitle = $chartDynamic.append('p.legend-title')
-    $legend = $chartDynamic.append('div.legend')
+    $legendTitle = $chartDynamicLegend.append('p.legend-title')
+    $legend = $chartDynamicLegend.append('div.legend')
     $legendItemLabelsContainer = $legend.append('p.legend-item-labels-container')
     $legendItemContainer = $legend.append('p.legend-item-container')
 
